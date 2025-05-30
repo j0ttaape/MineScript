a2053df18403df6dec4ec12ba08d10179755dcf2
j0ttaape <joaopedrotrajano59@gmail.com>
2025-05-30 19:59:56 -0300

    colocando as skills

--- funcoes.js
+++ funcoes.js
@@ -10,3 +10,35 @@ export function ApresentacaodoJogo(mensagem){
 ------------------------------------------------------------------------
                 `)
 }
+
+export function SelecaoDPersonagem(skil){
+    console.log(`------------------------------------------------------------------------
+            Qual vai ser seu tipo de skil?
+            1. Guerreiro
+                Vida: 450hp
+                Dano: 55
+
+            2. Mago
+                Vida: 300hp
+                Dano:35
+
+            3. Assasino
+                Vida: 375
+                Dano: 45
+            4. Desejo sair do game`);
+
+            let escolha = Number(ler());
+
+            if(skillJogado == 1){
+                skillJogado = 'Guerreiro'  
+            }
+            else if(skillJogado == 2){
+                skillJogado = 'Mago'
+            }
+            else if(skillJogado == 3){
+                skillJogado = 'Assasino'
+            }
+            else if(skillJogado == 4){
+                
+            }
+}
