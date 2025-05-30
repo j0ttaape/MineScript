d7b75f8203e79c54d4010ae3b2f947b7de464e7f
j0ttaape <joaopedrotrajano59@gmail.com>
2025-05-30 19:22:40 -0300

    apresentacao

---
+++ app.js
@@ -0,0 +1,6 @@
+import prompt from 'prompt-sync';
+import { ApresentacaodoJogo } from './funcoes.js';
+let ler = prompt();
+
+let apresentacao1 = ApresentacaodoJogo();
+
---
+++ funcoes.js
@@ -0,0 +1,12 @@
+
+
+// entrada de dados para o usuário
+export function ApresentacaodoJogo(mensagem){
+    console.log(`------------------------------------------------------------------------
+            Olá! Seja Bem-Vindo ao War-Script
+            
+    Estamos em guerra com Danzo, um monstro que está tomando as terras de Aphantom
+    nossa linha de frente precisa de você!
+------------------------------------------------------------------------
+                `)
+}
