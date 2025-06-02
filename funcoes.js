fe64450991be36d6cf65f5d883fb961409b8d468
j0ttaape <joaopedrotrajano59@gmail.com>
2025-06-02 08:03:22 -0300

    dano do steve

--- funcoes.js
+++ funcoes.js
@@ -2,28 +2,37 @@
 // -- Funções do Zumbi --
 export function danoZoombie(zumbi,steve){
     if(steve.armadura = 0){
-        zumbi.dano = math.parseint(random()*5)
+        zumbi.dano = math.parseint(random()*10)
     }
 
     else if(steve.armadura = 1){
 
-        zumbi.dano = math.parseint(random()*3.5)
+        zumbi.dano = math.parseint(random()*7.5)
     }
 
     else if(steve.armadura = 2){
 
-        zumbi.dano = math.parseint(random()*2)
+        zumbi.dano = math.parseint(random()*5)
     }
     return zumbi.dano
 }
 
 export function danoSteve(steve){
     if(steve.espada = 0){
+        steve.dano = parseint(math.random()*15)
+    }
+    
+  else if(steve.espada = 1){
         steve.dano = parseint(math.random()*30)
     }
     
-    if(steve.espada = 1){
-        steve.dano = parseint(math.random()*30)
+  else if(steve.espada = 2){
+        steve.dano = parseint(math.random()*45)
     }
-
+    
+  else if(steve.espada = 3){
+        steve.dano = parseint(math.random()*60)
+    }
+    return steve.dano
 }
+
