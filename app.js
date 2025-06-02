4de26e2d24f138ebc2f99f0b83c10ec91a627f30
j0ttaape <joaopedrotrajano59@gmail.com>
2025-06-02 08:22:57 -0300

    apresentacao do steve

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ let zumbi = {
 
 console.log(`\nBem-vindo você está no MineScript.
     \nSua missão é derrotar os mobs e enfrentar o boss final.
-    \nBoa sorte, pois você vai precisar!`)
+    \nBoa sorte, pois você vai precisar!`);
 
-console.log(`O seu `)
+console.log(`O seu primeiro inimigo é o zumbie.
+    \nOs atributos do zumbie são:
+    \n Vida: ${zumbi.vida}
+    Dano: entre 1 a 10
+    \n
+    Já você... começa sem nada, apenas com sua espada de madeira e sua armadura de couro.
+    Os atributos do seu personagem são:
+    Vida: ${steve.vida}
+    Dano: entre 1 e 15
+    Armadura: de couro
+    `)
+
