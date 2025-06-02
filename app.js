067b09791491f7448f54f74d57adf151b761d167
j0ttaape <joaopedrotrajano59@gmail.com>
2025-06-02 07:56:16 -0300

    função zoombie

---
+++ app.js
@@ -0,0 +1,13 @@
+let steve={
+    vida: 75,
+    armadura: 0,
+    arma: 0,
+    dano: 0
+}
+
+let zumbi = {
+    vida: 125,
+    dano: 0
+}
+    
+
---
+++ funcoes.js
@@ -0,0 +1,29 @@
+
+// -- Funções do Zumbi --
+export function danoZoombie(zumbi,steve){
+    if(steve.armadura = 0){
+        zumbi.dano = math.parseint(random()*5)
+    }
+
+    else if(steve.armadura = 1){
+
+        zumbi.dano = math.parseint(random()*3.5)
+    }
+
+    else if(steve.armadura = 2){
+
+        zumbi.dano = math.parseint(random()*2)
+    }
+    return zumbi.dano
+}
+
+export function danoSteve(steve){
+    if(steve.espada = 0){
+        steve.dano = parseint(math.random()*30)
+    }
+    
+    if(steve.espada = 1){
+        steve.dano = parseint(math.random()*30)
+    }
+
+}
